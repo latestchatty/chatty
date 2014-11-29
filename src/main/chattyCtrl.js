16c4a604e49a6c8c33b58e9b1167b82f3e95e3ee
Andy Christianson <nix@nixxed.com>
2014-11-28 22:37:28 -0600

    feat(chatty): Separate controller from service.
    Service now handles the model and updating of it. Chatty now just displays it. Added an infinite scroll feature to prevent excess initial load of threads causing a digest failure.

--- src/main/chattyCtrl.js
+++ src/main/chattyCtrl.js
@@ -1,131 +1,16 @@
 angular.module('chatty')
-    .controller('chattyCtrl', function($scope, $http) {
+    .controller('chattyCtrl', function($scope, $window, $document, chattyService) {
         $scope.threads = [];
         $scope.eventId = 0;
         $scope.error = null;
-        var postDb = {};
 
-        $http.get('http://winchatty.com/v2/getNewestEventId')
-            .success(function(data) {
-                $scope.eventId = data.eventId;
-            }).error(function(data) {
-                $scope.error = data;
-            });
+        chattyService.fullLoad().then(function(threads) {
+            $scope.threads = threads;
+        });
 
-        $http.get('https://winchatty.com/v2/getChatty')
-            .success(function(data) {
-                data.threads.forEach(function(thread) {
-                    $scope.threads.push(fixThread(thread));
-                });
-
-                waitForEvents();
-            }).error(function(data) {
-                $scope.error = data;
-            });
-
-        function waitForEvents() {
-            $http.get('http://winchatty.com/v2/waitForEvent?lastEventId=' + $scope.eventId)
-                .success(function(data) {
-                    $scope.eventId = data.lastEventId;
-
-                    data.events.forEach(newEvent);
-
-                    waitForEvents();
-                }).error(function(data) {
-                    $scope.error = data;
-                });
-        }
-
-        function newEvent(event) {
-            var thread, post;
-
-            if (event.eventType === 'newPost') {
-                if (event.eventData.post.parentId === 0) {
-                    console.log('New root post', event.eventData.post);
-                    thread = {
-                        threadId: event.eventData.threadId,
-                        rootPost: fixPost(event.eventData.post),
-                        posts: []
-                    };
-                    $scope.threads.unshift(thread);
-                    postDb[thread.threadId] = thread;
-                } else {
-                    console.log('New reply', event.eventData.post);
-                    var parent = postDb[event.eventData.post.parentId];
-                    if (parent) {
-                        post = fixPost(event.eventData.post);
-                        parent.posts.push(post);
-                        postDb[post.id] =post;
-                    } else {
-                        console.log('Parent post not found', event.eventData.post.parentId);
-                    }
-                }
-            } else if (event.eventType === 'categoryChange') {
-                console.log('Category change', event);
-                post = postDb[event.eventData.postId];
-                if (post) {
-                    if (event.eventData.category === 'nuked') {
-                        if (post.post) {
-                            _.pull(postDb, post.post);
-                        } else {
-                            _.pull($scope.threads, post.thread);
-                        }
-                    } else {
-                        post.post.category = event.eventData.category;
-                    }
-                } else {
-                    console.log('Post not found', event.eventData.postId);
-                }
-            } else {
-                console.log('New event', event);
+        $document.bind('scroll', _.debounce(function () {
+            if (($window.pageYOffset + $window.innerHeight) / $window.document.body.scrollHeight >= 0.75) {
+                $scope.$apply(chattyService.loadMore());
             }
-        }
-
-        function fixThread(thread) {
-            var posts = thread.posts;
-            thread.posts = [];
-
-            _.forEach(posts, function(post) {
-                //various post fixes
-                fixPost(post);
-
-                //figure out where post belongs
-                if (post.parentId === 0) {
-                    //root post
-                    thread.rootPost = post;
-
-                    //attach to master post list
-                    postDb[post.id] = thread;
-                } else {
-                    //attach to master post list
-                    postDb[post.id] = post;
-                }
-            });
-
-            //remove root to prevent iterating again
-            _.pull(posts, thread.rootPost);
-
-            //create nested replies
-            _.forEach(posts, function(post) {
-                var parent = postDb[post.parentId];
-                if (parent) {
-                    parent.posts.push(post);
-                } else {
-                    console.log('Orphan post: ', post);
-                }
-            });
-
-            return thread;
-        }
-
-        function fixPost(post) {
-            //create the one-liner used for reply view
-            var stripped = post.body.replace(/<[^>]+>/gm, '');
-            post.oneline = stripped.slice(0, 106) + (stripped.length > 106 ? '...' : '');
-
-            //create sub-post container
-            post.posts = [];
-
-            return post;
-        }
+        }, 250, { maxWait: 1000}));
     });
---
+++ src/main/chattyService.js
@@ -0,0 +1,182 @@
+angular.module('chatty')
+    .service('chattyService', function($q, $http, $timeout) {
+        var initialLoadCount = 15;
+        var scrollLoadCount = 10;
+        var lastEventId = 0;
+        var threads = [];
+        var visibleThreads = [];
+        var newThreads = [];
+        var postDb = {};
+        var chattyService = {};
+
+        chattyService.fullLoad = function fullLoad() {
+            var deferred = $q.defer();
+
+            $http.get('http://winchatty.com/v2/getNewestEventId')
+                .success(function(data) {
+                    lastEventId = data.eventId;
+                }).error(function(data) {
+                    deferred.reject(data);
+                });
+
+            $http.get('https://winchatty.com/v2/getChatty')
+                .success(function(data) {
+                    newThreads = data.threads;
+
+                    //preprocess some threads before resolving
+                    processNewThreads();
+                    visibleThreads = visibleThreads.concat(threads);
+
+                    deferred.resolve(visibleThreads);
+
+                    //process more after we've responded
+                    processRemainingThreads();
+                }).error(function(data) {
+                    deferred.reject(data);
+                });
+
+            return deferred.promise;
+        };
+
+        chattyService.loadMore = function loadMore() {
+            if (visibleThreads.length != threads.length) {
+                var count = 0;
+                _.each(threads, function(thread) {
+                    if (!_.contains(visibleThreads, thread)) {
+                        visibleThreads.push(thread);
+                        count++;
+                        return visibleThreads.length != threads.length && scrollLoadCount < 5;
+                    }
+                });
+            }
+        };
+
+        function processRemainingThreads() {
+            $timeout(function() {
+                processNewThreads();
+
+                if (newThreads.length > 0) {
+                    processRemainingThreads();
+                } else {
+                    waitForEvents();
+                }
+            }, 500);
+        }
+
+        function processNewThreads() {
+            var max = newThreads.length > initialLoadCount ? initialLoadCount : newThreads.length;
+                _.range(0, max).forEach(function() {
+                var thread = newThreads.pop();
+                var fixed = fixThread(thread);
+                threads.push(fixed);
+            });
+        }
+
+        function waitForEvents() {
+            $http.get('http://winchatty.com/v2/waitForEvent?lastEventId=' + lastEventId)
+                .success(function(data) {
+                    lastEventId = data.lastEventId;
+
+                    data.events.forEach(newEvent);
+
+                    waitForEvents();
+                }).error(function(data) {
+                    console.log('Error in waitForEvents: ', data);
+                });
+        }
+
+        function newEvent(event) {
+            var thread, post;
+
+            if (event.eventType === 'newPost') {
+                if (event.eventData.post.parentId === 0) {
+                    console.log('New root post', event.eventData.post);
+                    thread = {
+                        threadId: event.eventData.threadId,
+                        rootPost: fixPost(event.eventData.post),
+                        posts: []
+                    };
+                    threads.unshift(thread);
+                    postDb[thread.threadId] = thread;
+                } else {
+                    console.log('New reply', event.eventData.post);
+                    var parent = postDb[event.eventData.post.parentId];
+                    if (parent) {
+                        post = fixPost(event.eventData.post);
+                        parent.posts.push(post);
+                        postDb[post.id] =post;
+                    } else {
+                        console.log('Parent post not found', event.eventData.post.parentId);
+                    }
+                }
+            } else if (event.eventType === 'categoryChange') {
+                console.log('Category change', event);
+                post = postDb[event.eventData.postId];
+                if (post) {
+                    if (event.eventData.category === 'nuked') {
+                        if (post.post) {
+                            _.pull(postDb, post.post);
+                        } else {
+                            _.pull(threads, post.thread);
+                        }
+                    } else {
+                        post.post.category = event.eventData.category;
+                    }
+                } else {
+                    console.log('Post not found', event.eventData.postId);
+                }
+            } else {
+                console.log('New event', event);
+            }
+        }
+
+        function fixThread(thread) {
+            var posts = thread.posts;
+            thread.posts = [];
+
+            _.forEach(posts, function(post) {
+                //various post fixes
+                fixPost(post);
+
+                //figure out where post belongs
+                if (post.parentId === 0) {
+                    //root post
+                    thread.rootPost = post;
+
+                    //attach to master post list
+                    postDb[post.id] = thread;
+                } else {
+                    //attach to master post list
+                    postDb[post.id] = post;
+                }
+            });
+
+            //remove root to prevent iterating again
+            _.pull(posts, thread.rootPost);
+
+            //create nested replies
+            _.forEach(posts, function(post) {
+                var parent = postDb[post.parentId];
+                if (parent) {
+                    parent.posts.push(post);
+                } else {
+                    console.log('Orphan post: ', post);
+                }
+            });
+
+            return thread;
+        }
+
+        function fixPost(post) {
+            //create the one-liner used for reply view
+            var stripped = post.body.replace(/<[^>]+>/gm, '');
+            post.oneline = stripped.slice(0, 106) + (stripped.length > 106 ? '...' : '');
+
+            //create sub-post container
+            post.posts = [];
+
+            return post;
+        }
+
+        return chattyService;
+    });
