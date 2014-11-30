fc3a8e91bf389e216a2f0ba1e9a2148dae7b6a78
Andy Christianson <nix@nixxed.com>
2014-11-29 18:25:11 -0600

    fix(chatty): Enhance filter performance.
    Using filter:expression causes changes to the DOM for each change of the expression. However, using a watch and ng-hide, iterating across threads is much faster, and has almost no performance loss when unfiltering.

--- src/main/chattyCtrl.js
+++ src/main/chattyCtrl.js
@@ -1,10 +1,27 @@
 angular.module('chatty')
-    .controller('chattyCtrl', function($scope, chattyService) {
+    .controller('chattyCtrl', function($scope, $filter, chattyService) {
         $scope.threads = [];
         $scope.eventId = 0;
         $scope.error = null;
+        $scope.filterSet = false;
+        $scope.filterExpression = null;
 
         chattyService.fullLoad().then(function(threads) {
             $scope.threads = threads;
+        });
+
+        $scope.$watch('filterExpression', function() {
+            $scope.filterSet = false;
+            if ($scope.filterExpression) {
+                _.forEach($scope.threads, function(thread) {
+                    delete thread.visible;
+                });
+
+                var visibleThreads = $filter('filter')($scope.threads, $scope.filterExpression);
+                visibleThreads.forEach(function(thread) {
+                    thread.visible = true;
+                });
+                $scope.filterSet = true;
+            }
         });
     });
