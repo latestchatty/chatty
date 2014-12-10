22c0b3d6d25861c4b20db9e309bdb59f15116af7
Andy Christianson <nix@nixxed.com>
2014-12-09 22:14:14 -0600

    fix(chatty): Root posts not showing initial tag values.

--- src/main/services/modelService.js
+++ src/main/services/modelService.js
@@ -110,6 +110,7 @@ angular.module('chatty')
                 thread.date = rootPost.date;
                 thread.category = rootPost.category;
                 thread.body = rootPost.body;
+                thread.lols = rootPost.lols;
                 thread.expirePercent = ((((new Date().getTime()) - new Date(rootPost.date).getTime()) / 3600000) / 18) * 100;
             }
             thread.replyCount = threadPosts.length || 0;
