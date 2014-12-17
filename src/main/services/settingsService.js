6bac9cb32943d72b624f18c80f43e7f14531ba27
boarder2 <boarder2@gmail.com>
2014-12-17 21:03:09 +0000

    Refactor settings in prep for cloud storage.

--- src/main/services/settingsService.js
+++ src/main/services/settingsService.js
@@ -1,13 +1,21 @@
 angular.module('chatty')
     .service('settingsService', function($document, $location, localStorageService) {
         var settingsService = {};
-
+        
+        var USE_LOCAL_STORAGE = true;
+        
+        var settingsContainer = {
+            collapsedThreads: [],
+            tabs: []
+        };
+        
         settingsService.isEmbeddedInShacknews = function isEmbeddedInShacknews () {
             return $location.host().indexOf('shacknews.com') >= 0;
         };
 
-        var collapsedThreads = angular.fromJson(localStorageService.get('collapsedThreads')) || [];
+        //Credentials are special and will always be stored in local storage
         var credentials = angular.fromJson(localStorageService.get('credentials')) || {username: '', password: ''};
+
         if (settingsService.isEmbeddedInShacknews()) {
             //Get the username from the hidden shack element.
             var el = $document[0].getElementById('user_posts');
@@ -15,30 +23,29 @@ angular.module('chatty')
             credentials.username = !!el ? el.innerHTML || '' : '';
             credentials.password = '';
         }
-        var tabs = angular.fromJson(localStorageService.get('tabs')) || [];
 
         settingsService.isCollapsed = function isCollapsed(id) {
-            return collapsedThreads.indexOf(Number(id)) >= 0;
+            return settingsContainer.collapsedThreads.indexOf(Number(id)) >= 0;
         };
 
         settingsService.addCollapsed = function addCollapsed(id) {
-            if (collapsedThreads.indexOf(id) < 0) {
-                collapsedThreads.push(id);
+            if (settingsContainer.collapsedThreads.indexOf(id) < 0) {
+                settingsContainer.collapsedThreads.push(id);
 
-                localStorageService.set('collapsedThreads', collapsedThreads);
+                save();
             }
         };
 
         settingsService.removeCollapsed = function removeCollapsed(id) {
-            _.pull(collapsedThreads, id);
-            localStorageService.set('collapsedThreads', collapsedThreads);
+            _.pull(settingsContainer.collapsedThreads, id);
+            save();
         };
 
         settingsService.cleanCollapsed = function cleanCollapsed(posts) {
-            _.remove(collapsedThreads, function(id) {
+            _.remove(settingsContainer.collapsedThreads, function(id) {
                 return !posts[id];
             });
-            localStorageService.set('collapsedThreads', collapsedThreads);
+            save();
         };
 
 
@@ -68,28 +75,45 @@ angular.module('chatty')
 
 
         settingsService.getTabs = function getTabs() {
-            return tabs;
+            return settingsContainer.tabs;
         };
 
         settingsService.addTab = function addTab(tab) {
-            if (!_.find(tabs, {'filterExpression' : tab.filterExpression})) {
-                tabs.push(tab);
+            if (!_.find(settingsContainer.tabs, {'filterExpression' : tab.filterExpression})) {
+                settingsContainer.tabs.push(tab);
             }
             saveTabs();
         };
 
         settingsService.removeTab = function removeTab(tab) {
-            _.pull(tabs, tab);
+            _.pull(settingsContainer.tabs, tab);
             saveTabs();
         };
 
         function saveTabs() {
-            var clone = _.cloneDeep(tabs);
+            var clone = _.cloneDeep(settingsContainer.tabs);
             _.each(clone, function(tab) {
                 delete tab.selected;
             });
-            localStorageService.set('tabs', clone);
+            save();
         }
 
+        function load() {        
+            if(USE_LOCAL_STORAGE) {
+                var data = angular.fromJson(localStorageService.get('chattyData'));
+                if(data) {
+                    settingsContainer = data;
+                }
+            }
+        }
+        
+        function save() {
+            if(USE_LOCAL_STORAGE) {
+                localStorageService.set('chattyData', settingsContainer);
+            }
+        }
+        
+        load();
+        
         return settingsService;
     });
