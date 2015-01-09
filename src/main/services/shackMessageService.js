a55cb32392dfc5501c60398188c8b989c55d9671
Andy Christianson <nix@nixxed.com>
2015-01-08 20:45:40 -0600

    style(shackMessageService): Style updates.

--- src/main/services/shackMessageService.js
+++ src/main/services/shackMessageService.js
@@ -1,36 +1,35 @@
 angular.module('chatty')
     .service('shackMessageService', function($window, settingsService, apiService) {
-       var shackMessageService = {};
-       
-       shackMessageService.totalMessageCount = '...';
-       shackMessageService.unreadMessageCount = '...';
-       
-       shackMessageService.getTotalMessageCount = function () { 
-          return shackMessageService.totalMessageCount;
-       }
-       
-       shackMessageService.getUnreadMessageCount = function () { 
-          return shackMessageService.unreadMessageCount;
-       }
-       
-       shackMessageService.refresh = function () {
-          if(settingsService.isLoggedIn())
-          {
-             apiService.getTotalInboxCount(settingsService.getUsername(), settingsService.getPassword())
-               .success(function(data) {
-                 shackMessageService.totalMessageCount = data.total;
-                 shackMessageService.unreadMessageCount = data.unread;
-               })
-               .error(function (data) {
-                 shackMessageService.totalMessageCount = -1;
-                 shackMessageService.unreadMessageCount = -1;
-               });
-          }
-       };
-       
-       shackMessageService.goToInbox = function () {
-           $window.open("https://www.shacknews.com/messages", "_blank");
-       };
-       
-       return shackMessageService;
+        var shackMessageService = {};
+
+        shackMessageService.totalMessageCount = '...';
+        shackMessageService.unreadMessageCount = '...';
+
+        shackMessageService.getTotalMessageCount = function() {
+            return shackMessageService.totalMessageCount;
+        };
+
+        shackMessageService.getUnreadMessageCount = function() {
+            return shackMessageService.unreadMessageCount;
+        };
+
+        shackMessageService.refresh = function() {
+            if (settingsService.isLoggedIn()) {
+                apiService.getTotalInboxCount(settingsService.getUsername(), settingsService.getPassword())
+                    .success(function(data) {
+                        shackMessageService.totalMessageCount = data.total;
+                        shackMessageService.unreadMessageCount = data.unread;
+                    })
+                    .error(function(data) {
+                        shackMessageService.totalMessageCount = -1;
+                        shackMessageService.unreadMessageCount = -1;
+                    });
+            }
+        };
+
+        shackMessageService.goToInbox = function() {
+            $window.open('https://www.shacknews.com/messages', '_blank');
+        };
+
+        return shackMessageService;
     });
