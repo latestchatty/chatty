7a5eab275ce6cc8fdc13590b205b03db82460004
Andy Christianson <nix@nixxed.com>
2015-02-22 11:39:41 -0600

    feat(loading): Loading messages from list now. Also more one time bindings.

--- src/main/threads/threadsDirective.js
+++ src/main/threads/threadsDirective.js
@@ -8,9 +8,14 @@ angular.module('chatty')
                 //load full chatty on start
                 $scope.threads = modelService.getThreads();
                 $scope.newThreads = modelService.getNewThreads();
-                $scope.isLoaded = false;
+                $scope.loadingMessages = [
+                    'Think before you post...',
+                    'It gets you chicks!',
+                    '<span style="color: red; font-weight: bold;"> * N U K E D *</span>'
+                ];
+                $scope.loadingMessage = _.sample($scope.loadingMessages);
 
-                $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
+                $scope.$on('ngRepeatFinished', function() {
                     $scope.isLoaded = true;
                 });
 
