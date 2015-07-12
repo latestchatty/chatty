c4bcf7f076f5618af409b10f4c37eca7e2733e90
Andy Christianson <nix@nixxed.com>
2015-07-12 16:30:17 -0500

    feat(embed): Add vimeo embeds.

--- src/main/services/bodyTransformService.js
+++ src/main/services/bodyTransformService.js
@@ -5,6 +5,7 @@ angular.module('chatty')
         var matchers = [
             {regex: /<a[^<]+?href="([^"]+?\.(png|jpg|jpeg|gif))">[^<]+?<\/a>/gi, type: 'image'},
             {regex: /<a[^<]+?href="((https?:\/\/)?(www\.|m\.)?(youtube\.com|youtu\.be).+?)">[^<]+?<\/a>/gi, type: 'youtube'},
+            {regex: /<a[^<]+?href="((https?:\/\/)?(www\.|m\.)?vimeo\.com\/.+?)">[^<]+?<\/a>/gi, type: 'vimeo'},
             {regex: /<a[^<]+?href="((https?:\/\/)?(.+\.)?(imgur\.com).+?)">[^<]+?<\/a>/gi, type: 'imgur'},
             {regex: /<a[^<]+?href="([^"]+?gfycat\.com\/[^"]+?)">[^<]+?<\/a>/gi, type: 'gfycat'},
             {regex: /<a[^<]+?href="((https?:\/\/)?(www\.)?(shacknews\.com)\/chatty\?id=(\d+)(#item_(\d+))?.*?)">[^<]+?<\/a>/gi, type: 'comment'}
