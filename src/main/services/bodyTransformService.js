494c5e15fea83c7ff24c989268308d267dbd66f1
Andy Christianson <nix@nixxed.com>
2015-07-13 22:00:08 -0500

    fix(embed): Fix greedy imgur regex.

--- src/main/services/bodyTransformService.js
+++ src/main/services/bodyTransformService.js
@@ -6,7 +6,7 @@ angular.module('chatty')
             {regex: /<a[^<]+?href="([^"]+?\.(png|jpg|jpeg|gif))">[^<]+?<\/a>/gi, type: 'image'},
             {regex: /<a[^<]+?href="((https?:\/\/)?(www\.|m\.)?(youtube\.com|youtu\.be).+?)">[^<]+?<\/a>/gi, type: 'youtube'},
             {regex: /<a[^<]+?href="((https?:\/\/)?(www\.|m\.)?vimeo\.com\/.+?)">[^<]+?<\/a>/gi, type: 'vimeo'},
-            {regex: /<a[^<]+?href="((https?:\/\/)?(.+\.)?(imgur\.com).+?)">[^<]+?<\/a>/gi, type: 'imgur'},
+            {regex: /<a[^<]+?href="((https?:\/\/)?([A-Za-z]+\.)?(imgur\.com).+?)">[^<]+?<\/a>/gi, type: 'imgur'},
             {regex: /<a[^<]+?href="([^"]+?gfycat\.com\/[^"]+?)">[^<]+?<\/a>/gi, type: 'gfycat'},
             {regex: /<a[^<]+?href="((https?:\/\/)?(www\.)?(shacknews\.com)\/chatty\?id=(\d+)(#item_(\d+))?.*?)">[^<]+?<\/a>/gi, type: 'comment'}
         ]
