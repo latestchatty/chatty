bc49edb53908f1b94a259bd4fd5098d00ff97778
Andy Christianson <nix@nixxed.com>
2016-01-18 01:36:34 -0600

    fix(build): Switch to CDN for main dependencies for now.
    Current minified dependencies are not correct from npm. This will
    save a ton of initial download on the site.

--- src/gulp/gulp-config.js
+++ src/gulp/gulp-config.js
@@ -12,19 +12,20 @@ module.exports = {
         './src/main/**/*.scss'
     ],
 
+    //TODO get rid of this if we only use CDN?
     dependencyPaths: [
-        {dir: './node_modules/angular2/bundles/', out: 'angular2.js',
-            dev: 'angular2.dev.js', prod: 'angular2.js'},
-        {dir: './node_modules/angular2/bundles/', out: 'angular2-polyfills.js',
-            dev: 'angular2-polyfills.js', prod: 'angular2-polyfills.min.js'},
-        {dir: './node_modules/angular2/bundles/', out: 'angular2-http.js',
-            dev: 'http.dev.js', prod: 'http.js'},
-        {dir: './node_modules/angular2/bundles/', out: 'angular2-router.js',
-            dev: 'router.dev.js', prod: 'router.js'},
-        {dir: './node_modules/rxjs/bundles/', out: 'rx.js',
-            dev: 'Rx.js', prod: 'Rx.min.js'},
-        {dir: './node_modules/systemjs/dist/', out: 'system.js',
-            dev: 'system.src.js', prod: 'system.js'}
+        //{dir: './node_modules/angular2/bundles/', out: 'angular2.js',
+        //    dev: 'angular2.dev.js', prod: 'angular2.js'},
+        //{dir: './node_modules/angular2/bundles/', out: 'angular2-polyfills.js',
+        //    dev: 'angular2-polyfills.js', prod: 'angular2-polyfills.min.js'},
+        //{dir: './node_modules/angular2/bundles/', out: 'angular2-http.js',
+        //    dev: 'http.dev.js', prod: 'http.js'},
+        //{dir: './node_modules/angular2/bundles/', out: 'angular2-router.js',
+        //    dev: 'router.dev.js', prod: 'router.js'},
+        //{dir: './node_modules/rxjs/bundles/', out: 'rx.js',
+        //    dev: 'Rx.js', prod: 'Rx.min.js'},
+        //{dir: './node_modules/systemjs/dist/', out: 'system.js',
+        //    dev: 'system.src.js', prod: 'system.js'}
     ],
 
     staticPaths: [
