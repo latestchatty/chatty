f3a1558874da793cc744fd5d41da2092e1b3d7dd
Andy Christianson <nix@nixxed.com>
2018-09-03 10:42:00 -0500

    feat: Add badge to refresh button for new threads.

--- src/nav/RefreshButton.js
+++ src/nav/RefreshButton.js
@@ -2,18 +2,39 @@ import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import Tooltip from '@material-ui/core/Tooltip'
 import RefreshIcon from '@material-ui/icons/Refresh'
+import Badge from '@material-ui/core/Badge'
 import withChatty from '../context/chatty/withChatty'
+import {withStyles} from '@material-ui/core/styles'
 
 class RefreshButton extends React.Component {
     render() {
+        const {classes, newThreads} = this.props
+        const displayBadge = newThreads && newThreads.length > 0
+        const title = displayBadge ? `Refresh Thread Order (${newThreads.length} new threads)` : 'Refresh Thread Order'
         return (
-            <Tooltip disableFocusListener title='Refresh Thread Order' enterDelay={350}>
+            <Tooltip disableFocusListener title={title} enterDelay={350}>
                 <IconButton onClick={this.props.refreshChatty}>
-                    <RefreshIcon/>
+                    {
+                        displayBadge ?
+                            <Badge badgeContent={newThreads.length} color='secondary' classes={{badge: classes.badge}}>
+                                <RefreshIcon/>
+                            </Badge>
+                            : <RefreshIcon/>
+                    }
                 </IconButton>
             </Tooltip>
         )
     }
 }
 
-export default withChatty(RefreshButton)
+const styles = {
+    badge: {
+        top: 12
+    }
+}
+
+export default withChatty(
+    withStyles(styles)(
+        RefreshButton
+    )
+)
