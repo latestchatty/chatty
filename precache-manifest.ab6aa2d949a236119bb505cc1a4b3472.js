b950ab31e535023a10fa5610c42496a3458a259f
Andy Christianson <nix@nixxed.com>
2019-02-10 17:25:14 -0600

    Updates

---
+++ precache-manifest.ab6aa2d949a236119bb505cc1a4b3472.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7cfab87ec76d92799dcd",
+    "url": "/static/js/main.7cfab87e.chunk.js"
+  },
+  {
+    "revision": "14e7153b21755252412a",
+    "url": "/static/js/1.14e7153b.chunk.js"
+  },
+  {
+    "revision": "7cfab87ec76d92799dcd",
+    "url": "/static/css/main.5bd42f4c.chunk.css"
+  },
+  {
+    "revision": "1a32946d3d122ca9ff3e291b269c931a",
+    "url": "/index.html"
+  }
+];
