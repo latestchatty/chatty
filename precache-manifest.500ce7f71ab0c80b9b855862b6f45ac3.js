7f9f012ae0eb2700088133eeb449f2decf059b9f
Andy Christianson <nix@nixxed.com>
2019-02-12 11:08:22 -0600

    Updates

--- precache-manifest.500ce7f71ab0c80b9b855862b6f45ac3.js
+++ precache-manifest.500ce7f71ab0c80b9b855862b6f45ac3.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "7a0861635ce3378b55ef",
-    "url": "/static/js/main.7a086163.chunk.js"
+    "revision": "f56e849b439f56eb1428",
+    "url": "/static/js/main.f56e849b.chunk.js"
   },
   {
     "revision": "5b1b75e8130d172ddd3a",
     "url": "/static/js/1.5b1b75e8.chunk.js"
   },
   {
-    "revision": "7a0861635ce3378b55ef",
+    "revision": "f56e849b439f56eb1428",
     "url": "/static/css/main.2c2dcc50.chunk.css"
   },
   {
-    "revision": "fa1ebc4b1a9c388e03efac0ff4d0f3f9",
+    "revision": "887ac4fd0df17f1a715a68306edc4e2a",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2d029f8a0f3f20fbf406359181c655f.js"
+  "/precache-manifest.500ce7f71ab0c80b9b855862b6f45ac3.js"
 );
 
 workbox.clientsClaim();
