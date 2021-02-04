8c9244101f037ec2c1e7971487c2bfd59a65fc14
Andy Christianson <nix@nixxed.com>
2021-02-04 13:54:15 -0600

    fix: Filtering within comments should behave more as expected.

--- src/thread/Comments.js
+++ src/thread/Comments.js
@@ -1,49 +1,53 @@
-import React, {useContext} from 'react'
+import React, {useContext, useMemo} from 'react'
 import Post from './Post'
 import OneLine from './OneLine'
 import classnames from 'classnames'
 import FilterContext from '../context/filter/FilterContext'
 
 function Comments({className, thread = {}, parent = thread, onHide, onCollapseReply, onExpandReply, onOpenReplyBox, expandedReplyId, replyBoxOpenForId, onCloseReplyBox}) {
-    const {filterSettings} = useContext(FilterContext)
+    const {isPostVisible} = useContext(FilterContext)
+
+    const comments = useMemo(() => {
+        return thread.posts
+            .filter(post => post.parentId === parent.id)
+            .filter(post => isPostVisible(thread, post))
+    }, [isPostVisible, parent.id, thread])
+
     return (
         <ul className={classnames('comments', className)}>
             {
-                thread.posts
-                    .filter(post => post.parentId === parent.id)
-                    .filter(post => filterSettings.showCollapsed || post.markType !== 'collapsed')
-                    .map(post =>
-                        <li key={post.id}>
-                            {
-                                expandedReplyId === post.id
-                                    ? <Post
-                                        post={post}
-                                        thread={thread}
-                                        onHide={onHide}
-                                        onCollapse={onCollapseReply}
-                                        replyBoxOpenForId={replyBoxOpenForId}
-                                        onOpenReplyBox={onOpenReplyBox}
-                                        onCloseReplyBox={onCloseReplyBox}
-                                    />
-                                    : <OneLine
-                                        post={post}
-                                        thread={thread}
-                                        onExpandReply={onExpandReply}
-                                    />
-                            }
-                            <Comments
-                                thread={thread}
-                                parent={post}
-                                expandedReplyId={expandedReplyId}
-                                replyBoxOpenForId={replyBoxOpenForId}
-                                onExpandReply={onExpandReply}
-                                onHide={onHide}
-                                onCollapseReply={onCollapseReply}
-                                onOpenReplyBox={onOpenReplyBox}
-                                onCloseReplyBox={onCloseReplyBox}
-                            />
-                        </li>
-                    )
+                comments.map(post =>
+                    <li key={post.id}>
+                        {
+                            expandedReplyId === post.id
+                                ? <Post
+                                    post={post}
+                                    thread={thread}
+                                    onHide={onHide}
+                                    onCollapse={onCollapseReply}
+                                    replyBoxOpenForId={replyBoxOpenForId}
+                                    onOpenReplyBox={onOpenReplyBox}
+                                    onCloseReplyBox={onCloseReplyBox}
+                                />
+                                : <OneLine
+                                    post={post}
+                                    thread={thread}
+                                    onExpandReply={onExpandReply}
+                                />
+                        }
+                        <Comments
+                            thread={thread}
+                            parent={post}
+                            expandedReplyId={expandedReplyId}
+                            replyBoxOpenForId={replyBoxOpenForId}
+                            onExpandReply={onExpandReply}
+                            onHide={onHide}
+                            onCollapseReply={onCollapseReply}
+                            onOpenReplyBox={onOpenReplyBox}
+                            onCloseReplyBox={onCloseReplyBox}
+                        />
+                    </li>
+                )
             }
         </ul>
     )
